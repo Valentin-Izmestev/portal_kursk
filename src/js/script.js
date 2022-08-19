c32f1486599a36421b611737b099356a3854e48e
Valentin Izmestev <v.n.izmestev@mail.ru>
2022-08-19 12:23:18 +0300

    добавил кнопки для перемещения вверх или низ страницы, и хлебные крошки, а т.ж. поправил стили чекбоксов.

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,35 @@ arCastomSelect.forEach(select=>{
 });
  
 
+let haron = document.querySelector('.haron');
+let haronGoTopBtn = document.querySelector('.haron__go-top');
+let haronGoBottomBtn = document.querySelector('.haron__go-bottom');
+let header = document.querySelector('header.header');
+let footer = document.querySelector('footer.footer');
+
+
+
+haronGoTopBtn.addEventListener('click', function(){
+    window.scrollBy({
+        top: header.getBoundingClientRect().top,
+        behavior: 'smooth'
+      });
+});
+haronGoBottomBtn.addEventListener('click', function(){
+    window.scrollBy({
+        top: footer.getBoundingClientRect().top,
+        behavior: 'smooth'
+      });
+});
+document.addEventListener('scroll', ()=>{
+   console.log( );
+   if(document.documentElement.scrollTop > 145){
+    haron.classList.add('haron--show');
+   }else{
+    if(haron.classList.contains('haron--show')){
+        haron.classList.remove('haron--show');
+    }
+   }
+})
+ 
+ 
